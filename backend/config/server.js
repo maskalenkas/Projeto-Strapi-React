b6d43d0acc5c3062945ec6dfc25f3075f3f1440e
Arthur <maskalenkasth@gmail.com>
2021-03-29 22:14:46 -0300

    Reestruturação dos arquivos

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3b309204257dce743a899c7bfbc72cd9'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd36a0b2-fb4b-4870-afb7-890c707ca566'
+};
