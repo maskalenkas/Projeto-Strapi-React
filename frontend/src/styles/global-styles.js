5aecebdfbee0b2a797e5f0bd04c421dcaa8c126e
Arthur <maskalenkasth@gmail.com>
2021-04-15 04:08:32 -0300

    gridContent adicionado

--- frontend/src/styles/global-styles.js
+++ frontend/src/styles/global-styles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
- @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
@@ -17,5 +15,16 @@ export const GlobalStyles = createGlobalStyle`
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
+  }
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor};
   }
 `;
