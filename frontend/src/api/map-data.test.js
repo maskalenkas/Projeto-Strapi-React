63d66d0c460f5212333985f2cc70edc1d5c07da2
Arthur <maskalenkasth@gmail.com>
2021-04-22 03:15:23 -0300

    Mapeamento 1 feito

--- frontend/src/api/map-data.test.js
+++ frontend/src/api/map-data.test.js
@@ -6,14 +6,18 @@ describe('map-data', () => {
     expect(pagesData.footerHtml).toBe('');
     expect(pagesData.slug).toBe('');
     expect(pagesData.title).toBe('');
+    console.log('oi');
   });
 
   it('should map data if there are data', () => {
+    // o [0] é referente ao array que sera retornado. ELe quer o indice 0 do array que o MapData vai retornar
     const pagesData = mapData([
       {
         footer_text: '<p>Hey</p>',
         slug: 'slug',
         title: 'title',
+        sections: [],
+        menu: {},
       },
     ])[0];
     expect(pagesData.footerHtml).toBe('<p>Hey</p>');
